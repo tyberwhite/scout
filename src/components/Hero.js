17ce9625ef2f2cbad85c4087424d18c0b7109ea0
Jesse <zerocool@Jesses-MacBook-Pro.local>
2023-02-13 21:13:43 -0500

    Changed hero section font sizes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,10 @@ function Hero() {
     <div className="hero-section">
       <h1 className="hero-section-header">Real-time Geolocation & IP Lookup</h1>
       <p className="hero-section-sub-header">
-        Scout is a cutting-edge app that leverages the power of APIs to provide
-        real-time location data based on a user's IP address. With an
-        easy-to-use interface and accurate results, it is the perfect tool for
-        anyone looking to gather detailed information about their location and
-        the surrounding area.
+        Scout leverages the power of APIs to provide real-time location data
+        based on a user's IP address. With an easy-to-use interface and accurate
+        results, it is the perfect tool for anyone looking to gather detailed
+        information about their location and the surrounding area.
       </p>
     </div>
   );
